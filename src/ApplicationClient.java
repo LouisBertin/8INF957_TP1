d51e177bbfd48e3415aa39d1a7b93fde41b6d225
LouisBertin <bertin.louis7@gmail.com>
2019-01-24 14:35:56 -0500

    add teacher class

---
+++ src/ApplicationClient.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+
+public class ApplicationClient {
+
+    /**
+     * prend le fichier contenant la liste des commandes, et le charge dans une
+     * variable du type Commande qui est retournée
+     */
+    public Commande saisisCommande(BufferedReader fichier) {
+        // TODO : do something
+    }
+
+    /**
+     * initialise : ouvre les différents fichiers de lecture et écriture
+     */
+    public void initialise(String fichCommandes, String fichSortie) {
+        // TODO : do something
+    }
+
+    /**
+     * prend une Commande dûment formatée, et la fait exécuter par le serveur. Le résultat de
+     * l’exécution est retournée. Si la commande ne retourne pas de résultat, on retourne null.
+     * Chaque appel doit ouvrir une connexion, exécuter, et fermer la connexion. Si vous le
+     * souhaitez, vous pourriez écrire six fonctions spécialisées, une par type de commande
+     * décrit plus haut, qui seront appelées par  traiteCommande(Commande uneCommande)
+     */
+    public Object traiteCommande(Commande uneCommande) {
+        // TODO : do something
+    }
+
+
+    /**
+     * cette méthode vous sera fournie plus tard. Elle indiquera la séquence d’étapes à exécuter
+     * pour le test. Elle fera des appels successifs à saisisCommande(BufferedReader fichier) et
+     * traiteCommande(Commande uneCommande).
+     */
+    public void scenario() {
+        // TODO : do something
+    }
+
+    /**
+     * programme principal. Prend 4 arguments: 1) “hostname” du serveur, 2) numéro de port,
+     * 3) nom fichier commandes, et 4) nom fichier sortie. Cette méthode doit créer une
+     * instance de la classe ApplicationClient, l’initialiser, puis exécuter le scénario
+     */
+    public static void main(String[] args) {
+        // TODO : do something
+    }
+}
